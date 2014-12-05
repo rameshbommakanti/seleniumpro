95e99c4c6c354496123ca59ba4e359cd36fd9caf
venkat <venkat@venkat-PC>
2014-12-05 23:11:52 +0530

    Hi , i tried to write some methods , is this right. can you please explain me about method overloading and overrighting.

--- Testclass.java
+++ Testclass.java
@@ -1,4 +1,14 @@
 
 public class Testclass {
 	
+	public void Method1(int a, int b){
+		int sum=a+b;
+		System.out.println("Total value of two numbers:"+sum);
+		}
+	public void Method2(int a, int b, String c){
+		
+		int multiple=a*b;
+		String c="xxxxx";
+		System.out.println("The salary of the :"+c+ +multiple);
+	}
 }
